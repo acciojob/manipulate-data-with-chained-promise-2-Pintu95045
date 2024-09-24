88cef5757d57950a6e37f5e98f13da7973d035a3
Pintu Kumar Giri <131274412+Pintu95045@users.noreply.github.com>
2024-09-24 18:13:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,51 +1,27 @@
 //your JS code here. If required.
-function getNumbers() {
-return new Promise((resolve) => {
-setTimeout(() => {
-resolve([1, 2, 3, 4]);
-}, 3000);
-});
+function manipulateData(inputArray) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+      resolve(inputArray);
+    }, 3000);
+	}).then((array) => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				const evenNumbers = array.filter((num) => num % 2 === 0);
+				document.getElementById("output").innerText = evenNumbers.join(",");
+				resolve(evenNumbers);
+			},1000);
+		})
+	}).then((evenNumbers)=> {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				const doubleNumbers = evenNumbers.map((num) => num * 2);
+				document.getElementById("output").innerText = doubleNumbers.join(",");
+				resolve(doubleNumbers);
+			},2000);
+		})
+	});
 }
 
-// Function to filter out odd numbers
-function filterOdds(numbers) {
-return new Promise((resolve) => {
-setTimeout(() => {
-const evenNumbers = numbers.filter(number => number % 2 === 0);
-resolve(evenNumbers);
-}, 1000);
-});
-}
-
-// Function to multiply even numbers by 2
-function multiplyByTwo(numbers) {
-return new Promise((resolve) => {
-setTimeout(() => {
-const multipliedNumbers = numbers.map(number => number * 2);
-resolve(multipliedNumbers);
-}, 2000);
-});
-}
-
-// Function to update the DOM
-function updateOutput(text) {
-document.getElementById('output').textContent = text;
-}
-
-// Chain the promises
-getNumbers()
-.then(numbers => {
-return filterOdds(numbers).then(evenNumbers => {
-updateOutput(evenNumbers.join(', '));
-return evenNumbers;
-});
-})
-.then(evenNumbers => {
-return multiplyByTwo(evenNumbers).then(multipliedNumbers => {
-updateOutput(multipliedNumbers.join(', '));
-});
-})
-.catch(error => {
-console.error('An error occurred:', error);
-            });
-            });
+const inputArray = [1, 2, 3, 4];
+manipulateData(inputArray);
